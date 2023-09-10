a01a42ba707f53fd7d83ac87d56fbf233eaef354
annasiwiecka <117858832+annasiwiecka@users.noreply.github.com>
2023-09-10 16:27:15 +0200

    test

--- task_management/urls.py
+++ task_management/urls.py
@@ -4,10 +4,17 @@ from . import views
 urlpatterns = [
     path("", views.index, name="index"),
     
-    path("register/", views.register, name="register"),
-    path("login/", views.loginPage, name="login"),
-    path("logout/", views.logoutPage, name="logout"),
+    path("register", views.register, name="register"),
+    path("login", views.loginPage, name="login"),
+    path("logout", views.logoutPage, name="logout"),
     
-    path("home", views.home, name="home")
+    path("home", views.home, name="home"),
     
+    path("Profile"),
+    path("Notification"),
+    path("Team"),
+    path("Project"),
+    path("My-Task"),
+    path("Settings"),
+
 ]

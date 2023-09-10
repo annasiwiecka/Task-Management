781d48107d3edf1f761b39ebc93818f21fa4e7a9
annasiwiecka <117858832+annasiwiecka@users.noreply.github.com>
2023-09-10 22:20:29 +0200

    l

--- task_management/forms.py
+++ task_management/forms.py
@@ -2,8 +2,13 @@ from django import forms
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
 
+from .models import *
+
 
 class NewUserForm(UserCreationForm):
     class Meta:
         model = User
         fields = ("username", "email", "password1", "password2")
+
+class UpdateUserForm(forms.ModelForm):
+    username = forms.CharField(max_length=100)

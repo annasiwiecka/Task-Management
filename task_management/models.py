67a2aac3bda1090d0b562cb83bbec02cb830d6bd
annasiwiecka <117858832+annasiwiecka@users.noreply.github.com>
2023-09-19 02:01:27 +0200

    update database

--- task_management/models.py
+++ task_management/models.py
@@ -52,30 +52,80 @@ class TeamMember(models.Model):
 
     def __str__(self):
         return self.user.username
-    
-
 
 class CustomTeam(Group):
     team = models.ForeignKey(Team, on_delete=models.CASCADE)
 
 
-
 class Project(models.Model):
+    STATUS_CHOICES = [
+        ('Planning', 'Planning'),
+        ('In Progress', 'In Progress'),
+        ('Completed', 'Completed'),
+        ]
     name = models.CharField(max_length=50)
     description = models.CharField(max_length=2400)
-    team_id = models.ForeignKey(Team, on_delete=models.CASCADE)
+    team_members = models.ManyToManyField(User, related_name='projects')
     start = models.DateTimeField()
     end = models.DateTimeField()
-    status = models.CharField(max_length=20)
+    status = models.CharField(max_length=20, choices=STATUS_CHOICES, default='Planning')
+
+    def __str__(self):
+        return self.name
+    
+class Priority(models.Model):
+    HIGH = "High"
+    MEDIUM = 'Medium'
+    LOW = 'Low'
+    CRITICAL = 'Critical'
+    BLOCKER = 'Blocker'
+    
+    PRIORITY_CHOICES = [
+        (HIGH, 'High Priority'),
+        (MEDIUM, 'Medium Priority'),
+        (LOW, 'Low Priority'),
+        (CRITICAL, 'Critical Priority'),
+        (BLOCKER, 'Blocker Priority'),
+    ]
+    name = models.CharField(max_length=20, choices=PRIORITY_CHOICES, unique=True)
+    color = models.CharField(
+        max_length=7,
+        choices=[
+            ('#FF0000', 'Red'),
+            ('#FFA500', 'Orange'),
+            ('#FFFF00', 'Yellow'),
+            ('#800000', 'Dark Red'),
+            ('#000000', 'Black'),
+            ],
+            default='#FFFFFF',  # (white)
+        )
+
+    def __str__(self):
+        return self.name
+
+    def get_color(self):
+        return self.color
+    
+    class Meta:
+        verbose_name_plural = "Priorities"
 
 class Task(models.Model):
+    STATUS_CHOICES = [
+        ('Not Started', 'Not Started'),
+        ('In Progress', 'In Progress'),
+        ('Completed', 'Completed'),
+    ]
     name = models.CharField(max_length=200)
     description = models.CharField(max_length=2400)
     project_id = models.ForeignKey(Project, on_delete=models.CASCADE)
     assigned_to = models.ForeignKey(User, on_delete=models.CASCADE)
     deadline = models.DateTimeField()
-    priority = models.CharField(max_length=30)
-    status = models.CharField(max_length=15)
+    priority = models.ForeignKey(Priority, on_delete=models.PROTECT)
+    status = models.CharField(max_length=20, choices=STATUS_CHOICES, default='Not Started')
+
+    def __str__(self):
+        return self.name
+    
 
 class Comment(models.Model):
     task = models.ForeignKey(Task, on_delete=models.CASCADE, related_name='comments')

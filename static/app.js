e11ee215f25d837abd5f82fde24e4e40ef953363
annasiwiecka <117858832+annasiwiecka@users.noreply.github.com>
2023-10-23 16:19:53 +0200

    creating files for js

---
+++ static/app.js
@@ -0,0 +1,13 @@
+$(document).ready(function() {
+       
+    var currentPath = window.location.pathname;
+
+  
+    $(".nav-link").each(function() {
+        var linkPath = $(this).attr("href");
+        
+        if (currentPath === linkPath) {
+            $(this).closest(".nav-link").addClass("active");
+        }
+    });
+});
